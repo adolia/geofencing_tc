aa74fd34221a2b2ade539df647d80ff48d6c5601
Alex Dolia <aleksey.dolia@gmail.com>
2017-09-23 13:01:53 +0300

    add settings file with all options

---
+++ src/settings.py
@@ -0,0 +1,18 @@
+"""@package settings
+settings - module contains all geofencing configurable options
+This module contains all the configurable options of the geofencing validator tool.
+
+date Sep 23, 2017
+version 1.0
+
+Copyright (c) 2017 Topcoder Inc. All Rights Reserved.
+"""
+
+# The path to the tool log file
+LOG_FILE = 'log/geofencing_validator.log'
+
+# The log level value
+LOG_LEVEL = 'INFO'
+
+# The label for the validation result
+RESULT_LABEL = 'validation_result'
